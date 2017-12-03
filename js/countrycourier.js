8f8b1126dcf1e823c61e0a8bb1218cf042ef960b
Eric Lemonne <eric@c50.be>
2017-12-03 11:59:06 +0100

    Better strategy on regions

--- js/countrycourier.js
+++ js/countrycourier.js
@@ -20,28 +20,6 @@
 
     })();
 
-    //-- Sticky Header
-    (function() {
-
-      var mainnav = $('.header');
-
-      if (mainnav.length) {
-        var elmHeight = $('.header-top').outerHeight(true);
-        $(window).scroll(function() {
-
-          var scrolltop = $(window).scrollTop();
-          if (scrolltop > elmHeight) {
-            if (!mainnav.hasClass('sticky')) {
-              mainnav.addClass('sticky');
-            }
-
-          } else {
-            mainnav.removeClass('sticky');
-          }
-        })
-      }
-    })();
-
     //-- Flexslider
     (function() {
 
